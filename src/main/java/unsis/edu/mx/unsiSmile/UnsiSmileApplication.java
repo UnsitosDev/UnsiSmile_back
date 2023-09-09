5fbf552b44e0d137634e21416c816c16dee5068c
fr0ste <figueroamartinezjoelfrancisco@gmail.com>
2023-09-09 12:49:53 -0600

    se agregan primeras configuraciones de seguridad: auth en publico para la autenticacion

---
+++ src/main/java/unsis/edu/mx/unsiSmile/UnsiSmileApplication.java
@@ -0,0 +1,13 @@
+package unsis.edu.mx.unsiSmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsiSmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsiSmileApplication.class, args);
+	}
+
+}
---
+++ src/main/java/unsis/edu/mx/unsiSmile/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package unsis.edu.mx.unsiSmile.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+
+
+        return http
+                .csrf(csrf ->
+                        csrf
+                                .disable())
+                .authorizeHttpRequests(aunthRequest ->
+                        aunthRequest
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+
+
+    }
+}
