fe126dbeb4425920450d47897ef5a886863814a2
fr0ste <figueroamartinezjoelfrancisco@gmail.com>
2023-10-15 21:03:06 -0600

    se cambia el campo id numerico para manejar uuids incorporando mayor seguridad

--- src/main/java/edu/mx/unsis/unsiSmile/authenticationProviders/model/UserModel.java
+++ src/main/java/edu/mx/unsis/unsiSmile/authenticationProviders/model/UserModel.java
@@ -5,28 +5,47 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
 public class UserModel implements UserDetails {
     @Id
-    @GeneratedValue
-    private Long id;
+    @Column(columnDefinition = "BINARY(16)")
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private UUID id;
     @Column(nullable = false)
     String username;
     String password;
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     RoleModel role;
+
+    public UserModel(){
+        this.id = UUID.randomUUID();
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getIdAsString() {
+        return id.toString();
+    }
+
+    public void setIdFromString(String id) {
+        this.id = UUID.fromString(id);
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- src/main/java/edu/mx/unsis/unsiSmile/authenticationProviders/repositories/UserRepository.java
+++ src/main/java/edu/mx/unsis/unsiSmile/authenticationProviders/repositories/UserRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import edu.mx.unsis.unsiSmile.authenticationProviders.model.UserModel;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<UserModel,Long> {
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
     Optional<UserModel> findByUsername(String username);
 }
