d698e95e2e3bb62bea069a635ef63708239c1105
Skar13F <of5162@gmail.com>
2024-04-18 16:00:53 -0600

    crud for cycles

---
+++ src/main/java/edu/mx/unsis/unsiSmile/repository/ICycleRepository.java
@@ -0,0 +1,7 @@
+package edu.mx.unsis.unsiSmile.repository;
+
+import edu.mx.unsis.unsiSmile.model.CycleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICycleRepository extends JpaRepository<CycleModel,Long> {
+}
