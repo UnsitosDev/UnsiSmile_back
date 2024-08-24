23d7496e772c1afe37c521e2b25e60d246d56a6c
fr0ste <73338az@gmail.com>
2024-08-24 13:47:33 -0600

    refactor: se eliminan los modelos que ya no se utilizaran (se hará refactor)
    Se crea tambien la respectiva migración.

--- src/main/java/edu/mx/unsis/unsiSmile/repository/medicalHistories/IPeriodontogramRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.mx.unsis.unsiSmile.repository.medicalHistories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.mx.unsis.unsiSmile.model.medicalHistories.PeriodontogramModel;
-
-public interface IPeriodontogramRepository extends JpaRepository<PeriodontogramModel, Long> {
-}
---
+++ src/main/resources/db/migration/V0.0.5__changelog.sql
@@ -0,0 +1,14 @@
+drop table tooth_face;
+ALTER TABLE medical_histories
+DROP FOREIGN KEY FKgo8hepew1xjgf8saygcxios6c,
+DROP FOREIGN KEY FKrhhi1yjakls4yd90dwdrwrjl8;
+ALTER TABLE medical_histories
+DROP COLUMN fk_final_odontogram,
+DROP COLUMN fk_initial_odontogram;
+drop table odontogram;
+drop table tooth;
+drop table tooth_condition;
+drop table pocket_measurement_detail;
+drop table tooth_regions_periodontogram;
+drop table periodontogram;
+drop table dental_code;
