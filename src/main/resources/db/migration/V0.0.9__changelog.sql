a81cb9ff1a65efef915c3b6e74dedf3cf27ec8c2
Skar13F <of5162@gmail.com>
2024-09-08 13:13:12 -0600

    feat: Se elimina los drop table del changelog 9

--- src/main/resources/db/migration/V0.0.9__changelog.sql
+++ src/main/resources/db/migration/V0.0.9__changelog.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS clinical_history_catalogs;
 CREATE TABLE clinical_history_catalogs (
                                              id_clinical_history_catalog bigint(20) NOT NULL AUTO_INCREMENT,
                                              clinical_history_name varchar(100) NOT NULL,
@@ -6,7 +5,6 @@ CREATE TABLE clinical_history_catalogs (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- FormSections Table
-DROP TABLE IF EXISTS form_sections;
 CREATE TABLE form_sections (
                                id_form_section BIGINT(20) NOT NULL AUTO_INCREMENT,
                                form_name VARCHAR(100) UNIQUE NOT NULL,
@@ -16,7 +14,6 @@ CREATE TABLE form_sections (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ClinicalHistorySections Table
-DROP TABLE IF EXISTS clinical_history_sections;
 CREATE TABLE clinical_history_sections (
                                              fk_clinical_history_catalog bigint(20) NOT NULL,
                                              fk_form_section bigint(20) NOT NULL,
@@ -27,29 +24,26 @@ CREATE TABLE clinical_history_sections (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Type answer Table
-DROP TABLE IF EXISTS answer_types;
 CREATE TABLE answer_types (
-                                     id_answer_type INT AUTO_INCREMENT,
+                                     id_answer_type bigint(20) AUTO_INCREMENT,
                                      description VARCHAR(50) NOT NULL,
                                      PRIMARY KEY (id_answer_type)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;;
 
 -- Questions Table
-DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
                              id_question bigint(20) NOT NULL AUTO_INCREMENT,
                              question_text text NOT NULL,
                              fk_form_section bigint(20) NOT NULL,
-                             fk_answer_type INT NOT NULL,
+                             fk_answer_type bigint(20) NOT NULL,
                              PRIMARY KEY (id_question),
                              KEY fk_form_section (fk_form_section),
                              KEY fk_answer_type (fk_answer_type),
                              CONSTRAINT questions_ibfk_1 FOREIGN KEY (fk_form_section) REFERENCES form_sections (id_form_section),
-                             CONSTRAINT questions_ibfk_2 FOREIGN KEY (fk_answer_type) REFERENCES answer_type (id_answer_type)
+                             CONSTRAINT questions_ibfk_2 FOREIGN KEY (fk_answer_type) REFERENCES answer_types (id_answer_type)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Catalogs Table (for predefined answer catalogs)
-DROP TABLE IF EXISTS catalogs;
 CREATE TABLE catalogs (
                             id_catalog bigint(20) NOT NULL AUTO_INCREMENT,
                             catalog_name varchar(50) NOT NULL,
@@ -57,7 +51,6 @@ CREATE TABLE catalogs (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- CatalogOptions Table (options within a predefined catalog)
-DROP TABLE IF EXISTS catalog_options;
 CREATE TABLE catalog_options (
                                    id_catalog_option bigint(20) NOT NULL AUTO_INCREMENT,
                                    fk_catalog bigint(20) NOT NULL,
@@ -68,7 +61,6 @@ CREATE TABLE catalog_options (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Treatments Table
-DROP TABLE IF EXISTS patient_clinical_histories;
 CREATE TABLE patient_clinical_histories (
                                               id_patient_clinical_history bigint(20) NOT NULL AUTO_INCREMENT,
                                               fk_clinical_history_catalog bigint(20) DEFAULT NULL,
@@ -82,7 +74,6 @@ CREATE TABLE patient_clinical_histories (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Files Table
-DROP TABLE IF EXISTS files;
 CREATE TABLE files (
                        id_file BIGINT(20) NOT NULL AUTO_INCREMENT,
                        file_name VARCHAR(255) NOT NULL,
@@ -92,7 +83,6 @@ CREATE TABLE files (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Answers Table
-DROP TABLE IF EXISTS answers;
 CREATE TABLE answers (
                          id_answer BIGINT(20) NOT NULL AUTO_INCREMENT,
                          fk_patient_clinical_history BIGINT(20) NOT NULL,
