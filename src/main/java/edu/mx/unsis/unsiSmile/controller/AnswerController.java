6f6c5ee0f0b777b9aee39fd7ba7b29a9e4d4357f
Skar13F <of5162@gmail.com>
2024-09-28 00:47:16 -0600

    feat: Se agrega el servicio para procesar el llenado de un formulario

--- src/main/java/edu/mx/unsis/unsiSmile/controller/AnswerController.java
+++ src/main/java/edu/mx/unsis/unsiSmile/controller/AnswerController.java
@@ -12,29 +12,29 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "---------------ANSWERS")
+@Tag(name = "ANSWERS")
 @RestController
-@RequestMapping("/api/answers")
+@RequestMapping("/unsismile/api/answers")
 @RequiredArgsConstructor
 public class AnswerController {
 
     private final AnswerService answerService;
 
-    @Operation(summary = "Crea un registro respuesta, **necesita estar creada y relacionada con el paciente y el id de la pregunta a la que pertenece")
+    @Operation(summary = "Guarda la respuesta a una pregunta.")
     @PostMapping
     public ResponseEntity<AnswerResponse> save(@RequestBody AnswerRequest request) {
         AnswerResponse response = answerService.save(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @Operation(summary = "obtiene una respuesta por su id")
+    @Operation(summary = "Obtiene una respuesta por su id.")
     @GetMapping("/{id}")
     public ResponseEntity<AnswerResponse> findById(@PathVariable Long id) {
         AnswerResponse response = answerService.findById(id);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @Operation(summary = "Obtiene todas las respuestas, ** a futuro paginarlas para no tener problemas de procesamiento")
+    @Operation(summary = "Obtiene todas las respuestas en el sistema.")
     @GetMapping
     public ResponseEntity<List<AnswerResponse>> findAll() {
         List<AnswerResponse> response = answerService.findAll();
@@ -48,11 +48,19 @@ public class AnswerController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Operation(summary = "Obtiene todas las respuestas que corresponden a una hc de un paciente")
+    @Operation(summary = "Recupera todas las respuestas asociadas a la historia clínica de un paciente.")
     @GetMapping("/patient-clinical-history/{patientClinicalHistoryId}")
     public ResponseEntity<List<AnswerResponse>> getAnswersByPatientClinicalHistory(
             @PathVariable Long patientClinicalHistoryId) {
         List<AnswerResponse> response = answerService.getAnswersByPatientClinicalHistory(patientClinicalHistoryId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    @Operation(summary = "Crea múltiples registros de respuestas, para rellenar los formularios.")
+    @PostMapping("/batch")
+    public ResponseEntity<List<AnswerResponse>> saveBatch(@RequestBody List<AnswerRequest> requests) {
+        List<AnswerResponse> responses = answerService.saveBatch(requests);
+        return new ResponseEntity<>(responses, HttpStatus.CREATED);
+    }
+
 }
