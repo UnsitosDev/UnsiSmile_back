4ab3dedafa7b9218659dab0624c5d7c0886614ac
fr0ste <73338az@gmail.com>
2024-10-19 15:55:08 -0600

    Fix: se agregan questions faltantes.

---
+++ src/main/resources/db/migration/V0.0.9__changelog.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+    questions (
+    question_text,
+    fk_form_section,
+    fk_answer_type,
+    question_order,
+    required
+)
+values
+    ("Dientes Cariados", 24, 3, 1, true),
+    ("Amalgamas", 24, 3, 2, true),
+    ("Dientes Ausentes", 24, 3, 3, true),
+    ("Resinas", 24, 3, 4, true),
+    ("Dientes Obturados", 24, 3, 5, true),
+    ("Incrustaciones", 24, 3, 6, true),
+    ("Extracciones Indicadas", 24, 3, 7, true),
+    ("Prótesis Fija", 24, 3, 8, true),
+    ("Raíces", 24, 3, 9, true),
+    ("Prótesis Removible", 24, 3, 10, true);
