7caa834527caa1889c81c0964cec799e30f79067
fr0ste <73338az@gmail.com>
2024-10-19 19:53:34 -0600

    Feat: se agrega el poblado para hc de periodoncia.

--- src/main/resources/db/migration/V0.0.8__changelog.sql
+++ src/main/resources/db/migration/V0.0.8__changelog.sql
@@ -318,6 +318,7 @@ VALUES
     ("Zona Apical", 33, 4, 6, 2, true),
     ("Conducto Radicular", 34, 4, 7, 3, true);
 
+
 INSERT INTO
     questions (
     question_text,
@@ -330,3 +331,23 @@ INSERT INTO
 VALUES
     ("Numero de Conductos", 35, 4, 8, 1, true),
     ("Proporción Corona-Raíz", 36, 4, 9, 2, true);
+
+INSERT INTO
+    questions (
+    question_text,
+    fk_form_section,
+    fk_answer_type,
+    question_order,
+    required
+)
+values
+    ("Dientes Cariados", 24, 3, 1, true),
+    ("Amalgamas", 24, 3, 2, true),
+    ("Dientes Ausentes", 24, 3, 3, true),
+    ("Resinas", 24, 3, 4, true),
+    ("Dientes Obturados", 24, 3, 5, true),
+    ("Incrustaciones", 24, 3, 6, true),
+    ("Extracciones Indicadas", 24, 3, 7, true),
+    ("Prótesis Fija", 24, 3, 8, true),
+    ("Raíces", 24, 3, 9, true),
+    ("Prótesis Removible", 24, 3, 10, true);
