f3cee0b8e479ae9a67e6d60ba09f21c8a35d59a5
Oscar Fuentes <of5162@gmail.com>
2024-10-20 22:49:26 -0600

    feat: Se implementa un search jpql para filtrar student(s).

--- src/main/java/edu/mx/unsis/unsiSmile/repository/students/IStudentRepository.java
+++ src/main/java/edu/mx/unsis/unsiSmile/repository/students/IStudentRepository.java
@@ -1,12 +1,15 @@
 package edu.mx.unsis.unsiSmile.repository.students;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import edu.mx.unsis.unsiSmile.authenticationProviders.model.UserModel;
 import edu.mx.unsis.unsiSmile.model.students.StudentModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface IStudentRepository extends JpaRepository<StudentModel, String> {
@@ -14,4 +17,22 @@ public interface IStudentRepository extends JpaRepository<StudentModel, String> 
         Optional<StudentModel> findByEnrollmentAndStatusKey(String enrollment, String statusKey);
 
         Optional<StudentModel> findByUser(UserModel user);
+
+        @Query("SELECT s FROM StudentModel s WHERE s.enrollment LIKE %:searchInput% " +
+                "OR s.person.curp LIKE %:searchInput% " +
+                "OR s.person.firstName LIKE %:searchInput% " +
+                "OR s.person.secondName LIKE %:searchInput% " +
+                "OR s.person.firstLastName LIKE %:searchInput% " +
+                "OR s.person.secondLastName LIKE %:searchInput% " +
+                "OR s.person.phone LIKE %:searchInput% " +
+                "OR s.person.email LIKE %:searchInput% " +
+                "OR s.person.gender.gender LIKE %:searchInput% " +
+                "OR (YEAR(s.person.birthDate) = :searchInputInt " +
+                "OR MONTH(s.person.birthDate) = :searchInputInt " +
+                "OR DAY(s.person.birthDate) = :searchInputInt) " +
+                "AND s.statusKey='A'")
+        Page<StudentModel> findAllBySearchInput(
+                @Param("searchInput") String searchInput,
+                @Param("searchInputInt") Integer searchInputInt,
+                Pageable pageable);
 }
