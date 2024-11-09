1a40451263b968dbf777b9ec9d14493e7e4bd06d
Oscar Fuentes <of5162@gmail.com>
2024-11-09 12:10:31 -0600

    feat: Se implementa un search con jpql para filtrar localidades.

--- src/main/java/edu/mx/unsis/unsiSmile/repository/addresses/ILocalityRepository.java
+++ src/main/java/edu/mx/unsis/unsiSmile/repository/addresses/ILocalityRepository.java
@@ -3,7 +3,11 @@ package edu.mx.unsis.unsiSmile.repository.addresses;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import edu.mx.unsis.unsiSmile.model.addresses.LocalityModel;
@@ -20,4 +24,6 @@ public interface ILocalityRepository extends JpaRepository<LocalityModel, String
 
     List<LocalityModel> findByMunicipality(MunicipalityModel municipality);
 
+    @Query("SELECT l FROM LocalityModel l WHERE l.name like :keyword%")
+    Page<LocalityModel> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
--- src/main/java/edu/mx/unsis/unsiSmile/service/addresses/LocalityService.java
+++ src/main/java/edu/mx/unsis/unsiSmile/service/addresses/LocalityService.java
@@ -3,6 +3,8 @@ package edu.mx.unsis.unsiSmile.service.addresses;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -94,12 +96,17 @@ public class LocalityService {
     }
 
     @Transactional(readOnly = true)
-    public List<LocalityResponse> getAllLocalities() {
+    public Page<LocalityResponse> getAllLocalities(Pageable pageable, String keyword) {
         try {
-            List<LocalityModel> allLocalities = localityRepository.findAll();
-            return allLocalities.stream()
-                    .map(localityMapper::toDto)
-                    .collect(Collectors.toList());
+            Page<LocalityModel> localities;
+
+            if (keyword != null && !keyword.isEmpty()) {
+                localities = localityRepository.findByKeyword(keyword, pageable);
+            } else {
+                localities = localityRepository.findAll(pageable);
+            }
+
+            return localities.map(localityMapper::toDto);
         } catch (Exception ex) {
             throw new AppException("Failed to fetch all localities", HttpStatus.INTERNAL_SERVER_ERROR, ex);
         }
