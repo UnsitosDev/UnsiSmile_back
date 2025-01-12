c0ef7e5298cf09feaf83a6108afbefff0226fe08
fr0ste <73338az@gmail.com>
2025-01-11 21:51:05 -0700

    fix(config): Update CORS configuration to restrict allowed origins

--- src/main/java/edu/mx/unsis/unsiSmile/config/WebConfig.java
+++ src/main/java/edu/mx/unsis/unsiSmile/config/WebConfig.java
@@ -1,49 +1,49 @@
-    package edu.mx.unsis.unsiSmile.config;
+package edu.mx.unsis.unsiSmile.config;
 
-    import java.util.Arrays;
+import java.util.Arrays;
 
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.context.annotation.Configuration;
-    import org.springframework.http.HttpHeaders;
-    import org.springframework.http.HttpMethod;
-    import org.springframework.web.cors.CorsConfiguration;
-    import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-    import org.springframework.web.filter.CorsFilter;
-    import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-    import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-    import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-    @Configuration
-    @EnableWebMvc
-    public class WebConfig implements WebMvcConfigurer {
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
-        @Bean
-        public CorsFilter corsFilter() {
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            CorsConfiguration config = new CorsConfiguration();
-            config.setAllowCredentials(true);
-            config.setAllowedOriginPatterns(Arrays.asList("*"));     
-            config.setAllowedHeaders(Arrays.asList(
-                    HttpHeaders.AUTHORIZATION,
-                    HttpHeaders.CONTENT_TYPE,
-                    HttpHeaders.ACCEPT
-            ));
-            config.setAllowedMethods(Arrays.asList(
-                    HttpMethod.GET.name(),
-                    HttpMethod.POST.name(),
-                    HttpMethod.PUT.name(),
-                    HttpMethod.PATCH.name(),
-                    HttpMethod.DELETE.name()
-            ));
-            config.setMaxAge(3600L);
-            source.registerCorsConfiguration("/**", config);
-            return new CorsFilter(source);
-        }
-
-        @Override
-        public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("upload/**")
-                    .addResourceLocations("file:/upload/");
-        }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:8081",
+                "https://unsismile.unsis.edu.mx",
+                "http://132.18.41.181:8081"));
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT));
+        config.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.DELETE.name()));
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
-    
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("upload/**")
+                .addResourceLocations("file:/upload/");
+    }
+}
