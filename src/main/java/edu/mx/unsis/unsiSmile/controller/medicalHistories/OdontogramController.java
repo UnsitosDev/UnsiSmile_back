292b0cd5e48bc58562e765177ec2b68069c07c2a
fr0ste <73338az@gmail.com>
2025-02-21 19:29:43 -0700

    refactor: remove RegionMeasurementPockets related classes and controller

--- src/main/java/edu/mx/unsis/unsiSmile/controller/medicalHistories/OdontogramController.java
+++ src/main/java/edu/mx/unsis/unsiSmile/controller/medicalHistories/OdontogramController.java
@@ -1,21 +1,26 @@
 package edu.mx.unsis.unsiSmile.controller.medicalHistories;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import edu.mx.unsis.unsiSmile.dtos.request.medicalHistories.OdontogramRequest;
 import edu.mx.unsis.unsiSmile.dtos.response.medicalHistories.OdontogramResponse;
 import edu.mx.unsis.unsiSmile.service.medicalHistories.OdontogramService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/unsismile/api/v1/medical-histories/odontograms")
-public class OdontogramController { 
+public class OdontogramController {
 
     private final OdontogramService odontogramService;
 
@@ -37,7 +42,7 @@ public class OdontogramController {
 
     @PutMapping("/{id}")
     public ResponseEntity<OdontogramResponse> updateOdontogram(@Valid @PathVariable Long id,
-                                                                @Valid @RequestBody OdontogramRequest updateRequest) {
+            @Valid @RequestBody OdontogramRequest updateRequest) {
         OdontogramResponse updatedOdontogramResponse = odontogramService.updateOdontogram(id, updateRequest);
         return ResponseEntity.ok(updatedOdontogramResponse);
     }
@@ -59,11 +64,13 @@ public class OdontogramController {
         return odontogramService.getOdontogramDetails(patientId);
     }
 
-    //obtener un odontograma por el id del formulario y el id del paciente
+    // obtener un odontograma por el id del formulario y el id del paciente
     @GetMapping("/form-section/{formSectionId}/patient/{patientId}")
-    public ResponseEntity<OdontogramResponse> getOdontogramByFormSectionIdAndPatientId(@PathVariable Long formSectionId, @PathVariable String patientId) {
-        OdontogramResponse odontogramResponse = odontogramService.getOdontogramByFormSectionId(formSectionId, patientId);
+    public ResponseEntity<OdontogramResponse> getOdontogramByFormSectionIdAndPatientId(@PathVariable Long formSectionId,
+            @PathVariable String patientId) {
+        OdontogramResponse odontogramResponse = odontogramService.getOdontogramByFormSectionId(formSectionId,
+                patientId);
         return ResponseEntity.ok(odontogramResponse);
     }
-    
+
 }
