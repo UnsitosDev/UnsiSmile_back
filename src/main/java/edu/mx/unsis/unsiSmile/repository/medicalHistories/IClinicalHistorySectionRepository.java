9af7ceff54110add555537e584279cfa20b83da8
Oscar Fuentes <of5162@gmail.com>
2025-02-22 14:12:35 -0600

    refactor: update ClinicalHistorySectionRepository to use composite key and adjust service methods accordingly

--- src/main/java/edu/mx/unsis/unsiSmile/repository/medicalHistories/IClinicalHistorySectionRepository.java
+++ src/main/java/edu/mx/unsis/unsiSmile/repository/medicalHistories/IClinicalHistorySectionRepository.java
@@ -1,6 +1,7 @@
 package edu.mx.unsis.unsiSmile.repository.medicalHistories;
 
 import edu.mx.unsis.unsiSmile.model.ClinicalHistorySectionModel;
+import edu.mx.unsis.unsiSmile.model.utils.ClinicalHistorySectionModelPk;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface IClinicalHistorySectionRepository extends JpaRepository<ClinicalHistorySectionModel, Long> {
+public interface IClinicalHistorySectionRepository extends JpaRepository<ClinicalHistorySectionModel, ClinicalHistorySectionModelPk> {
     @Query("SELECT chs FROM ClinicalHistorySectionModel chs WHERE chs.clinicalHistoryCatalogModel.idClinicalHistoryCatalog = :clinicalHistoryId")
     List<ClinicalHistorySectionModel> findAllByClinicalHistoryId(@Param("clinicalHistoryId") Long clinicalHistoryId);
 }
