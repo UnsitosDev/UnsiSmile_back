dfedc0138ad82698a191ec370f724cff870e7305
Oscar Fuentes <of5162@gmail.com>
2025-02-24 13:18:25 -0600

    feat: add email configuration and Thymeleaf template resolver; update application properties and database migration for password token

---
+++ src/main/java/edu/mx/unsis/unsiSmile/config/MailConfig.java
@@ -0,0 +1,45 @@
+package edu.mx.unsis.unsiSmile.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private int mailPort;
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
+    @Value("${spring.mail.properties.mail.smtp.ssl.trust}")
+    private String mailSmtpSslTrust;
+
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(mailHost);
+        mailSender.setPort(mailPort);
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", String.valueOf(true));
+        props.put("mail.smtp.starttls.enable", String.valueOf(true));
+        props.put("mail.smtp.ssl.trust", mailSmtpSslTrust);
+
+        return mailSender;
+    }
+}
