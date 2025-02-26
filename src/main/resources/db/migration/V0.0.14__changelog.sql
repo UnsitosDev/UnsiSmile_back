748420e6fbe673f9738185f8c269d1acfc38e9c4
Elio <elio.antonio.654@gmail.com>
2025-02-26 12:47:43 -0600

    refactor(migration): update question and catalog option entries for consistency

--- src/main/resources/db/migration/V0.0.14__changelog.sql
+++ src/main/resources/db/migration/V0.0.14__changelog.sql
@@ -38,21 +38,6 @@ VALUES
     ("Catedráticos responsables de área");
 
 
--- datos personales
-INSERT INTO 
-    questions (
-    question_text,
-    fk_form_section,
-    fk_answer_type,
-    fk_catalog,
-    question_order,
-    required,
-    placeholder
-)
-VALUES
-    ("Estado civil de los padres (si es menor de edad)", 48, 4, 12, 1, false, null),
-    ("Nombre del pediatra o médico familiar", 48, 3, null, 2, false, "Nombre");
-
 INSERT INTO catalog_options
 (fk_catalog,
  option_name)
@@ -76,12 +61,12 @@ INSERT INTO
     placeholder 
 )
 VALUES
-    ("Diente 16", 54, 3, 1, false, "Código"),
-    ("Diente 11", 54, 3, 2, false, "Código"),
-    ("Diente 26", 54, 3, 3, false, "Código"),
-    ("Diente 46", 54, 3, 4, false, "Código"),
-    ("Diente 31", 54, 3, 5, false, "Código"),
-    ("Diente 36", 54, 3, 6, false, "Código");
+    ("Diente 16", 53, 3, 1, false, "Código"),
+    ("Diente 11", 53, 3, 2, false, "Código"),
+    ("Diente 26", 53, 3, 3, false, "Código"),
+    ("Diente 46", 53, 3, 4, false, "Código"),
+    ("Diente 31", 53, 3, 5, false, "Código"),
+    ("Diente 36", 53, 3, 6, false, "Código");
 
 -- profilaxis dental sesiones 
 INSERT INTO 
@@ -93,7 +78,7 @@ INSERT INTO
     required
 )
 VALUES
-    ("Fotografias Sesiones", 50, 6, 1, false);
+    ("Fotografias Sesiones", 49, 6, 1, false);
 ;
 
 -- Fluorosis
@@ -107,12 +92,12 @@ INSERT INTO
     placeholder 
 )
 VALUES
-    ("Diente 13", 51, 3, 1, false, "Código"),
-    ("Diente 12", 51, 3, 2, false, "Código"),
-    ("Diente 11", 51, 3, 3, false, "Código"),
-    ("Diente 21", 51, 3, 4, false, "Código"),
-    ("Diente 22", 51, 3, 5, false, "Código"),
-    ("Diente 23", 51, 3, 6, false, "Código");
+    ("Diente 13", 50, 3, 1, false, "Código"),
+    ("Diente 12", 50, 3, 2, false, "Código"),
+    ("Diente 11", 50, 3, 3, false, "Código"),
+    ("Diente 21", 50, 3, 4, false, "Código"),
+    ("Diente 22", 50, 3, 5, false, "Código"),
+    ("Diente 23", 50, 3, 6, false, "Código");
 
 -- BARNIZ O SELLADORES DE FOSETAS Y FISURAS
 INSERT INTO 
@@ -125,14 +110,14 @@ INSERT INTO
     placeholder 
 )
 VALUES
-    ("Autorización", 52, 3, 1, false, "Descripción"),
-    ("Aislamiento relativo", 52, 3, 2, false, "Descripción"),
-    ("Limpieza de superficie", 52, 3, 3, false, "Descripción"),
-    ("Grabado", 52, 3, 4, false, "Descripción"),
-    ("Lavado y secado", 52, 3, 5, false, "Descripción"),
-    ("Sellador de foseta y fisura", 52, 3, 6, false, "Descripción"),
-    ("Terminado", 52, 3, 7, false, "Descripción"),
-     ("Fotografias Barniz o selladores de fosetas y fisuras", 52, 6, 8, false, null);
+    ("Autorización", 51, 3, 1, false, "Descripción"),
+    ("Aislamiento relativo", 51, 3, 2, false, "Descripción"),
+    ("Limpieza de superficie", 51, 3, 3, false, "Descripción"),
+    ("Grabado", 51, 3, 4, false, "Descripción"),
+    ("Lavado y secado", 51, 3, 5, false, "Descripción"),
+    ("Sellador de foseta y fisura", 51, 3, 6, false, "Descripción"),
+    ("Terminado", 51, 3, 7, false, "Descripción"),
+    ("Fotografias Barniz o selladores de fosetas y fisuras", 51, 6, 8, false, null);
 
 -- Observaciones
 
@@ -146,10 +131,10 @@ INSERT INTO
     fk_catalog
 )
 VALUES
-    ("Fecha Aplicación Tópica de Flúor ", 53, 9, 1, false, null),
-    ("Nombre del operador", 53, 3, 2, false, null),
-    ("Nombre del asistente", 53, 3, 3, false, null),
-    ("Docente que autoriza", 53, 4, 4, true, 13);
+    ("Fecha Aplicación Tópica de Flúor ", 52, 9, 1, false, null),
+    ("Nombre del operador", 52, 3, 2, false, null),
+    ("Nombre del asistente", 52, 3, 3, false, null),
+    ("Docente que autoriza", 52, 4, 4, true, 13);
 
 -- PROFILAXIS / DETARTRAJE
 INSERT INTO questions
@@ -161,8 +146,8 @@ INSERT INTO questions
  placeholder,
  fk_catalog)
 VALUES
-    ("Enseño al paciente las caras teñidas por la pastilla reveladora", 55, 1, 1, false, null, null),
-    ("Explico al paciente uso de la pastilla reveladora", 55, 1, 2, false, null, null),
-    ("Elimino la placa dentobacteriana o cálculo de los dientes", 55, 1, 3, false, null, null),
-    ("Explico al paciente técnica de cepillado", 55, 1, 4, false, null, null);
+    ("Enseño al paciente las caras teñidas por la pastilla reveladora", 54, 1, 1, false, null, null),
+    ("Explico al paciente uso de la pastilla reveladora", 54, 1, 2, false, null, null),
+    ("Elimino la placa dentobacteriana o cálculo de los dientes", 54, 1, 3, false, null, null),
+    ("Explico al paciente técnica de cepillado", 54, 1, 4, false, null, null);
 
