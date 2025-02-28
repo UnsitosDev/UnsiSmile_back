4fe6d857b92b96d9986fa336b9ddecdb0e5325b5
Joel Francisco Figueroa Martínez <71165314+fr0ste@users.noreply.github.com>
2025-02-27 17:51:06 -0700

    feat(occupations): implement pagination and search functionality for occupations. (#178)

--- src/main/java/edu/mx/unsis/unsiSmile/controller/patients/OccupationController.java
+++ src/main/java/edu/mx/unsis/unsiSmile/controller/patients/OccupationController.java
@@ -1,17 +1,24 @@
 package edu.mx.unsis.unsiSmile.controller.patients;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import edu.mx.unsis.unsiSmile.dtos.request.patients.OccupationRequest;
 import edu.mx.unsis.unsiSmile.dtos.response.patients.OccupationResponse;
 import edu.mx.unsis.unsiSmile.service.patients.OccupationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/unsismile/api/v1/occupations")
@@ -43,15 +50,16 @@ public class OccupationController {
 
     @Operation(summary = "Obtener una lista paginada de ocupaciones")
     @GetMapping
-    public ResponseEntity<Page<OccupationResponse>> getAllStudents(
+    public ResponseEntity<Page<OccupationResponse>> getAllOccupations(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "occupation") String order,
-            @RequestParam(defaultValue = "true") boolean asc) {
-        Sort sort = asc ? Sort.by(order).ascending() : Sort.by(order).descending();
-        Pageable pageable = PageRequest.of(page, size, sort);
-        Page<OccupationResponse> occupationResponses = occupationService.getAllOccupations(pageable);
-        return ResponseEntity.ok(occupationResponses);
+            @RequestParam(defaultValue = "true") boolean asc,
+            @Parameter(description = "Optional parameter to specify a search criterion.")
+            @RequestParam(required = false) String keyword) {
+        Page<OccupationResponse> occupations = 
+            occupationService.getAllOccupations(page, size, order, asc, keyword);
+        return ResponseEntity.ok(occupations);
     }
 
     @PutMapping("/{id}")
