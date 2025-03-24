e03a60cc35841fee1175a9c4770bb9deeb4bdc52
Oscar Fuentes <of5162@gmail.com>
2025-03-23 18:44:23 -0600

    feat: agregar nuevos mensajes de respuesta para el estado del semestre y actualizar la validación en StudentRequest

--- src/main/java/edu/mx/unsis/unsiSmile/common/ResponseMessages.java
+++ src/main/java/edu/mx/unsis/unsiSmile/common/ResponseMessages.java
@@ -270,4 +270,7 @@ public class ResponseMessages {
     public static final String ERROR_FETCHING_STATUS_LIST = "Error al obtener la lista de estados de la historia clínica";
 
     public static final String INVALID_STATUS = "Estatus no válido: ";
+
+    public static final String SEMESTER_NOT_FOUND = "Semestre no encontrado";
+    public static final String CURRENT_SEMESTER_INACTIVE = "No hay semestre activo";
 }
