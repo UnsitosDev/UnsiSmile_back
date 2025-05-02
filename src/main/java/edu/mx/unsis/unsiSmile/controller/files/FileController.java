b057f637f38b7892316f0f2a94b83074d2992429
Oscar Fuentes <of5162@gmail.com>
2025-05-01 22:17:34 -0600

    feat: enhance FileController with general file management endpoints

--- src/main/java/edu/mx/unsis/unsiSmile/controller/files/FileController.java
+++ src/main/java/edu/mx/unsis/unsiSmile/controller/files/FileController.java
@@ -1,24 +1,19 @@
 package edu.mx.unsis.unsiSmile.controller.files;
 
-import java.util.List;
-
+import edu.mx.unsis.unsiSmile.dtos.response.FileResponse;
+import edu.mx.unsis.unsiSmile.service.files.FileService;
+import edu.mx.unsis.unsiSmile.service.files.GeneralFileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import edu.mx.unsis.unsiSmile.dtos.response.FileResponse;
-import edu.mx.unsis.unsiSmile.service.files.FileService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @Tag(name = "FILE")
 @RestController
@@ -27,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class FileController {
 
     private final FileService fileService;
+    private final GeneralFileService generalFileService;
 
     @Operation(summary = "Crear un archivo, necesita una respuesta creada")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -52,11 +48,30 @@ public class FileController {
     }
 
     @Operation(summary = "Carga los archivos generales para descarga y llenado (consentimiento/asentimiento informado)")
-    @PostMapping("/general")
+    @PostMapping(value = "/general-files", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> uploadGeneralFiles(
-            @RequestPart List<MultipartFile> files,
-            @RequestPart @Validated String idQuestion) {
-        fileService.uploadGeneralFiles(files, Long.parseLong(idQuestion));
+            @RequestPart List<MultipartFile> files) {
+        generalFileService.uploadGeneralFiles(files);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "Obtiene todos los archivos generales")
+    @GetMapping("general-files")
+    public ResponseEntity<List<FileResponse>> getAllGeneralFiles() {
+        List<FileResponse> response = generalFileService.getAllGeneralFiles();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Obtiene un archivo general para su descarga")
+    @GetMapping("general-file/{id}")
+    public ResponseEntity<byte[]> downloadGeneralFile(@PathVariable String id) {
+        return generalFileService.downloadGeneralFileById(id);
+    }
+
+    @Operation(summary = "Elimina un archivo general")
+    @DeleteMapping("general-file/{id}")
+    public ResponseEntity<Void> deleteGeneralFile(@PathVariable String id) {
+        generalFileService.deleteGeneralFileById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
