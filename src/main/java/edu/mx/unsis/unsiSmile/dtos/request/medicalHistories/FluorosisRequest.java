c34fca28d5896e0389ee4e7b260bdb7be98a235c
Oscar Fuentes <of5162@gmail.com>
2025-05-06 14:55:06 -0600

    feat: add FluorosisRequest and FluorosisResponse DTOs for medical history management

---
+++ src/main/java/edu/mx/unsis/unsiSmile/dtos/request/medicalHistories/FluorosisRequest.java
@@ -0,0 +1,31 @@
+package edu.mx.unsis.unsiSmile.dtos.request.medicalHistories;
+
+import edu.mx.unsis.unsiSmile.common.ResponseMessages;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FluorosisRequest {
+    @NotNull(message = ResponseMessages.REQUEST_CANNOT_BE_NULL)
+    private List<ToothRequest> theetFluorosis;
+
+    @NotNull(message = ResponseMessages.PATIENT_ID_CANNOT_BE_NULL)
+    private String idPatient;
+
+    @NotNull(message = ResponseMessages.QUESTION_ID_CANNOT_BE_NULL)
+    private Long idQuestion;
+
+    @NotNull(message = ResponseMessages.CLINICAL_HISTORY_ID_CANNOT_BE_NULL)
+    private Long idPatientMedicalRecord;
+
+    @NotNull(message = ResponseMessages.FORM_SECTION_ID_CANNOT_BE_NULL)
+    private Long idFormSection;
+}
