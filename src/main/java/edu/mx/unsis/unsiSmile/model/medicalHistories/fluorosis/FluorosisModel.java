fe929c180a3bec9ff10b7365682300e9b520ba5c
Oscar Fuentes <of5162@gmail.com>
2025-05-06 14:42:12 -0600

    feat: add Fluorosis and related models for medical history management

---
+++ src/main/java/edu/mx/unsis/unsiSmile/model/medicalHistories/fluorosis/FluorosisModel.java
@@ -0,0 +1,39 @@
+package edu.mx.unsis.unsiSmile.model.medicalHistories.fluorosis;
+
+import edu.mx.unsis.unsiSmile.model.FormSectionModel;
+import edu.mx.unsis.unsiSmile.model.patients.PatientModel;
+import edu.mx.unsis.unsiSmile.model.utils.AuditModel;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "fluorosis", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"fk_patient", "fk_form_section"})
+})
+public class FluorosisModel extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_fluorosis")
+    private Long idFluorosis;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_patient", referencedColumnName = "id_patient")
+    private PatientModel patient;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_form_section", referencedColumnName = "id_form_section")
+    private FormSectionModel formSection;
+
+    @OneToMany(mappedBy = "fluorosis", cascade = CascadeType.MERGE, orphanRemoval = true)
+    private List<FluorosisToothConditionAssignmentModel> toothConditionAssignments;
+
+    @OneToMany(mappedBy = "fluorosis", cascade = CascadeType.MERGE, orphanRemoval = true)
+    private List<FluorosisToothfaceConditionsAssignmentModel> toothFaceConditionsAssignments;
+}
