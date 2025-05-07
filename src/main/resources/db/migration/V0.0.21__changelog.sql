9d643e2d43b3434305ab3916d14fe5a5347a01ca
Oscar Fuentes <of5162@gmail.com>
2025-05-07 15:08:10 -0600

    feat: implement FluorosisController,  and migration

---
+++ src/main/resources/db/migration/V0.0.21__changelog.sql
@@ -0,0 +1,75 @@
+INSERT INTO
+    form_sections (form_name, requires_review)
+VALUES ("Fluorosis", false);
+
+INSERT INTO
+    questions (question_text, fk_form_section, fk_answer_type, question_order, required)
+values("Sección contestada", 57, 3, 1, true);
+
+-- Fluorosis
+CREATE TABLE fluorosis (
+    id_fluorosis BIGINT AUTO_INCREMENT PRIMARY KEY,
+    fk_patient CHAR(36) NOT NULL,
+    fk_form_section BIGINT NOT NULL,
+    created_at DATETIME(6) DEFAULT NULL,
+    created_by VARCHAR(255) DEFAULT NULL,
+    status_key VARCHAR(255) DEFAULT NULL,
+    updated_at DATETIME(6) DEFAULT NULL,
+    updated_by VARCHAR(255) DEFAULT NULL,
+    FOREIGN KEY (fk_patient) REFERENCES patients (id_patient),
+    FOREIGN KEY (fk_form_section) REFERENCES form_sections (id_form_section)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE fluorosis_tooth_condition_assignments (
+    fluorosis_id BIGINT,
+    tooth_id VARCHAR(3),
+    tooth_condition_id BIGINT,
+    created_at DATETIME(6) DEFAULT NULL,
+    created_by VARCHAR(255) DEFAULT NULL,
+    status_key VARCHAR(255) DEFAULT NULL,
+    updated_at DATETIME(6) DEFAULT NULL,
+    updated_by VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (fluorosis_id, tooth_id, tooth_condition_id),
+    FOREIGN KEY (fluorosis_id) REFERENCES fluorosis (id_fluorosis),
+    FOREIGN KEY (tooth_id) REFERENCES teeth (id_tooth),
+    FOREIGN KEY (tooth_condition_id) REFERENCES tooth_conditions (id_tooth_condition)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE fluorosis_toothface_conditions_assignments (
+    fluorosis_id BIGINT,
+    tooth_face_id VARCHAR(3),
+    toothface_condition_id BIGINT,
+    tooth_id VARCHAR(3),
+    created_at DATETIME(6) DEFAULT NULL,
+    created_by VARCHAR(255) DEFAULT NULL,
+    status_key VARCHAR(255) DEFAULT NULL,
+    updated_at DATETIME(6) DEFAULT NULL,
+    updated_by VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (
+                 fluorosis_id,
+                 tooth_face_id,
+                 toothface_condition_id,
+                 tooth_id
+                ),
+    FOREIGN KEY (fluorosis_id) REFERENCES fluorosis (id_fluorosis),
+    FOREIGN KEY (tooth_face_id) REFERENCES tooth_faces (id_tooth_face),
+    FOREIGN KEY (toothface_condition_id) REFERENCES toothface_conditions (id_toothface_conditions),
+    FOREIGN KEY (tooth_id) REFERENCES teeth (id_tooth)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+INSERT INTO form_component_tooth_conditions (fk_form_component, fk_tooth_condition)
+VALUES
+    (3, 3),
+    (3, 13);
+
+INSERT INTO form_component_tooth_faces (fk_form_component, fk_tooth_face)
+VALUES
+    (3, "1"),
+    (3, "2"),
+    (3, "3"),
+    (3, "4"),
+    (3, "5");
+
+INSERT INTO form_component_toothface_conditions (fk_form_component, fk_toothface_condition)
+VALUES
+    (3, 5);
