f2b2f29d0e5eaf6d28edcbabd53287b528b52fd9
Oscar Fuentes <of5162@gmail.com>
2025-05-19 20:25:47 -0600

    feat: add SOHI and SOHIToothCode models with corresponding database tables

--- src/main/resources/db/migration/V0.0.19__changelog.sql
+++ src/main/resources/db/migration/V0.0.19__changelog.sql
@@ -86,3 +86,29 @@ VALUES
 INSERT INTO form_component_tooth_conditions (fk_form_component, fk_tooth_condition)
 VALUES
     (1, 14); -- Nueva condición para el odontograma
+
+
+CREATE TABLE sohi (
+                      id_sohi BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
+                      fk_treatment_detail BIGINT(20) NOT NULL,
+                      created_at DATETIME(6) DEFAULT NULL,
+                      created_by VARCHAR(255) DEFAULT NULL,
+                      status_key VARCHAR(255) DEFAULT NULL,
+                      updated_at DATETIME(6) DEFAULT NULL,
+                      updated_by VARCHAR(255) DEFAULT NULL,
+                      FOREIGN KEY (fk_treatment_detail) REFERENCES treatment_details (id_treatment_detail)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE sohi_tooth_code (
+                                 id_tooth_code BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
+                                 fk_tooth VARCHAR(3) NOT NULL,
+                                 code INT NOT NULL,
+                                 sohi_id BIGINT(20) NOT NULL,
+                                 created_at DATETIME(6) DEFAULT NULL,
+                                 created_by VARCHAR(255) DEFAULT NULL,
+                                 status_key VARCHAR(255) DEFAULT NULL,
+                                 updated_at DATETIME(6) DEFAULT NULL,
+                                 updated_by VARCHAR(255) DEFAULT NULL,
+                                 FOREIGN KEY (sohi_id) REFERENCES sohi (id_sohi),
+                                 FOREIGN KEY (fk_tooth) REFERENCES teeth(id_tooth)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
