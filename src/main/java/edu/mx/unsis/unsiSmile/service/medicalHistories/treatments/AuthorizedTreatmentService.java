d9def12c16bcbfc61fc6e30d3df8ec39a2a5de6a
Oscar Fuentes <of5162@gmail.com>
2025-06-21 12:20:01 -0600

    refactor: Se actualiza el service de autorizacion para incluir datos del profesor en la respuesta

--- src/main/java/edu/mx/unsis/unsiSmile/service/medicalHistories/treatments/AuthorizedTreatmentService.java
+++ src/main/java/edu/mx/unsis/unsiSmile/service/medicalHistories/treatments/AuthorizedTreatmentService.java
@@ -7,6 +7,8 @@ import edu.mx.unsis.unsiSmile.model.medicalHistories.treatments.AuthorizedTreatm
 import edu.mx.unsis.unsiSmile.model.medicalHistories.treatments.TreatmentDetailModel;
 import edu.mx.unsis.unsiSmile.model.professors.ProfessorClinicalAreaModel;
 import edu.mx.unsis.unsiSmile.repository.medicalHistories.treatments.IAuthorizedTreatmentRepository;
+import edu.mx.unsis.unsiSmile.repository.medicalHistories.treatments.ITreatmentDetailRepository;
+import edu.mx.unsis.unsiSmile.repository.professors.IProfessorClinicalAreaRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,21 +22,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthorizedTreatmentService {
 
     private final IAuthorizedTreatmentRepository authorizedTreatmentRepository;
+    private final ITreatmentDetailRepository treatmentDetailRepository;
+    private final IProfessorClinicalAreaRepository professorClinicalAreaRepository;
 
     @Transactional
-    public void createAuthorizedTreatment(TreatmentStatusRequest request) {
+    public AuthorizedTreatmentModel createAuthorizedTreatment(TreatmentStatusRequest request) {
         try {
+            TreatmentDetailModel treatmentDetail = treatmentDetailRepository.findById(request.getTreatmentDetailId())
+                    .orElseThrow(() -> new AppException(
+                            String.format(ResponseMessages.TREATMENT_DETAIL_NOT_FOUND, request.getTreatmentDetailId()),
+                            HttpStatus.NOT_FOUND));
+
+            ProfessorClinicalAreaModel professorClinicalAreaModel = professorClinicalAreaRepository.findById(
+                    request.getProfessorClinicalAreaId())
+                    .orElseThrow(() -> new AppException(ResponseMessages.PROFESSOR_CLINICAL_AREA_NOT_FOUND, HttpStatus.NOT_FOUND));
+
             AuthorizedTreatmentModel model = AuthorizedTreatmentModel.builder()
-                    .treatmentDetail(TreatmentDetailModel.builder()
-                            .idTreatmentDetail(request.getTreatmentDetailId())
-                            .build())
+                    .treatmentDetail(treatmentDetail)
                     .status(request.getStatus())
-                    .professorClinicalArea(ProfessorClinicalAreaModel.builder()
-                            .idProfessorClinicalArea(request.getProfessorClinicalAreaId())
-                            .build())
+                    .professorClinicalArea(professorClinicalAreaModel)
                     .build();
 
-            authorizedTreatmentRepository.save(model);
+            return authorizedTreatmentRepository.save(model);
         } catch (Exception ex) {
             log.error(ResponseMessages.ERROR_CREATING_AUTHORIZED_TREATMENT, ex);
             throw new AppException(ResponseMessages.ERROR_CREATING_AUTHORIZED_TREATMENT, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -42,7 +51,7 @@ public class AuthorizedTreatmentService {
     }
 
     @Transactional
-    public void updateAuthorizedTreatmentByTreatmentId(TreatmentStatusRequest request) {
+    public AuthorizedTreatmentModel updateAuthorizedTreatmentByTreatmentId(TreatmentStatusRequest request) {
         try {
             AuthorizedTreatmentModel authorizedTreatment = authorizedTreatmentRepository
                     .findTopByTreatmentDetail_IdTreatmentDetailOrderByIdAuthorizedTreatmentDesc(request.getTreatmentDetailId())
@@ -50,11 +59,13 @@ public class AuthorizedTreatmentService {
                             String.format(ResponseMessages.AUTHORIZATION_REQUEST_NOT_FOUND, request.getTreatmentDetailId()),
                             HttpStatus.NOT_FOUND));
 
-            authorizedTreatment.setProfessorClinicalArea(ProfessorClinicalAreaModel.builder()
-                    .idProfessorClinicalArea(request.getProfessorClinicalAreaId())
-                    .build());
+            ProfessorClinicalAreaModel professorClinicalAreaModel = professorClinicalAreaRepository.findById(
+                            request.getProfessorClinicalAreaId())
+                    .orElseThrow(() -> new AppException(ResponseMessages.PROFESSOR_CLINICAL_AREA_NOT_FOUND, HttpStatus.NOT_FOUND));
+            professorClinicalAreaModel.getProfessor().getPerson().getFullName();
+            authorizedTreatment.setProfessorClinicalArea(professorClinicalAreaModel);
 
-            authorizedTreatmentRepository.save(authorizedTreatment);
+            return authorizedTreatmentRepository.save(authorizedTreatment);
         } catch (AppException e) {
             log.warn(String.format(ResponseMessages.AUTHORIZED_TREATMENT_NOT_FOUND, request.getTreatmentDetailId()));
             throw e;
