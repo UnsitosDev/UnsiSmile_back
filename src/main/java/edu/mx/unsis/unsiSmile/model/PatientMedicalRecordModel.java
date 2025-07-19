dadb84bdefca465405b3135ac780e0c7cc053c10
Oscar Fuentes <of5162@gmail.com>
2025-07-19 16:43:16 -0600

    refactor: actualizar las referencias en el modelo, repositorio y servicio de PatientClinicalHistory a PatientMedicalRecord para gestionar los registros médicos de los pacientes

--- src/main/java/edu/mx/unsis/unsiSmile/model/PatientMedicalRecordModel.java
+++ src/main/java/edu/mx/unsis/unsiSmile/model/PatientMedicalRecordModel.java
@@ -13,13 +13,13 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "patient_clinical_histories")
-public class PatientClinicalHistoryModel extends AuditModel {
+@Table(name = "patient_medical_records")
+public class PatientMedicalRecordModel extends AuditModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_patient_clinical_history")
-    private Long idPatientClinicalHistory;
+    @Column(name = "id_patient_medical_record")
+    private Long idPatientMedicalRecord;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fk_medical_record_catalog")
